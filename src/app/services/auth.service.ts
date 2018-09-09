a4a8223099d415050417d98964a20e79479b0220
= <=>
2018-09-09 10:28:16 +0300

    Implement user services

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { environment as env } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  public setToken(token: string, expires: number): void {
+    const now = Date.now();
+    const till = now + expires;
+    localStorage.setItem(env.tokenStorage.fieldVal, token);
+    localStorage.setItem(env.tokenStorage.fieldExp, till.toString());
+    // TODO make sure that local storage value was set
+  }
+
+  public get isLoggedIn(): boolean {
+    const token = localStorage.getItem(env.tokenStorage.fieldVal);
+    const exp = localStorage.getItem(env.tokenStorage.fieldExp);
+    const now = Date.now();
+    return (token && exp && now < +exp);
+  }
+
+  public get token(): string {
+    return localStorage.getItem(env.tokenStorage.fieldVal);
+  }
+
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/internal/operators';
+import { environment } from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly path: string = `${ environment.apiUrl }/api/user`;
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
+
+
+  public signup(form: any): Observable<any> {
+    return this.http.post(`${ this.path }/signup`, form).pipe(
+      tap((body: any) => {
+        if (body.token) {
+          this.authService.setToken(body.token, body.expires);
+        }
+      })
+    );
+  }
+}
